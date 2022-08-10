7872850d4fee5772a5b4f190f1653d21f42e599c
Elenilson <elenilsonsantos9@gmail.com>
2022-08-09 22:46:07 -0300

    exercicios resolvidos

---
+++ script.js
@@ -0,0 +1,10 @@
+const   idadeDependente = prompt('solicitação de dependente \n qual a idade do dependente?')
+
+if(idadeDependente >=13){
+if(idadeDependente<18)
+console.log('seu fiho ja pode ter um cartão vinculado ao seu')
+}else{
+    console.log('consulte outras possibilidades do labank')
+}
+else{
+    console.log('consulte outras possibilidades do labank')}
